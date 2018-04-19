e435b8fe80fdd82220b34f0238a9791cbcfe5b6e
karthikganapathi <karthik.ganapathi91@gmail.com>
2018-04-20 01:15:27 +0530

    hello world pro

--- src/Hello/helloworld.java
+++ src/Hello/helloworld.java
@@ -4,6 +4,11 @@ public class helloworld {
 	public static void main(String[] args) {
 		
 		
+		 for (int i=0;i<10;i++){
+			 System.out.println("Helo world "+i);
+			 
+		 }
+		
 		System.out.println("This is Git Hub project");
 		System.out.println("Hi world");
 		
